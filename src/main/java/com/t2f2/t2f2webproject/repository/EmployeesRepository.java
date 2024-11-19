a7bdc57f80147fca88aeed9063dc4c3286595fc8
dahee <ekgmltkfkd0@naver.com>
2024-11-20 02:02:55 +0900

    repository 오류 수정

--- src/main/java/com/t2f2/t2f2webproject/repository/EmployeesRepository.java
+++ src/main/java/com/t2f2/t2f2webproject/repository/EmployeesRepository.java
@@ -6,9 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface EmployeesRepository extends JpaRepository<EmployeesEntity, Integer> {
-    // 특정 팀에 속한 직원 목록 조회
-    List<EmployeesEntity> findByTeamName(String teamName);
 
-    // 이름과 ID로 직원 검색
-    EmployeesEntity findByNameAndId(String name);
 }
--- src/main/java/com/t2f2/t2f2webproject/repository/FileMovementsRepository.java
+++ src/main/java/com/t2f2/t2f2webproject/repository/FileMovementsRepository.java
@@ -7,9 +7,5 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface FileMovementsRepository extends JpaRepository<FileMovementsEntity, Integer> {
-    // 특정 컴퓨터에서 발생한 파일 이동 조회
-    List<FileMovementsEntity> findByComputersEntityIdx(Integer computerIdx);
 
-    // 특정 시간대에 발생한 파일 이동 조회
-    List<FileMovementsEntity> findByMoveTimeBetween(LocalDateTime start, LocalDateTime end);
 }
