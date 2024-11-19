c68fc618e7e30deb3c30e6e7662b4dd9439bf2ee
dahee <ekgmltkfkd0@naver.com>
2024-11-20 01:04:57 +0900

    project

---
+++ src/main/java/com/t2f2/t2f2webproject/T2F2WebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.t2f2.t2f2webproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2F2WebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2F2WebProjectApplication.class, args);
+	}
+
+}
